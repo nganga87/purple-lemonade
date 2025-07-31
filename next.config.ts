7e52e1024bec24b7f203b2a607a879dec9fbc008
Nicholas Nganga <ngangasondu@gmail.com>
2025-07-31 14:39:29 +0000

    {   "logName": "projects/addresschain/logs/run.googleapis.com%2Frequests

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/.git/:path*',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
